dbcb2c8ae62adb1d949d3349731934c7f3f196e7
Jeffrey <jeffrey@leagueoftraders.io>
2020-11-20 15:51:22 +0900

    Update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 var RaiToken = artifacts.require("RaiToken");
+var MasterChef = artifacts.require("MasterChef");
+var Migrator = artifacts.require("Migrator");
+var RaiBar = artifacts.require("RaiBar");
+var RaiMaker = artifacts.require("RaiMaker");
+var RaiRestaurant = artifacts.require("RaiRestaurant");
 
 module.exports = function (deployer) {
+  deployer.deploy(MasterChef);
   deployer.deploy(RaiToken);
+  deployer.deploy(Migrator);
+  deployer.deploy(RaiBar);
+  deployer.deploy(RaiMaker);
+  deployer.deploy(RaiRestaurant);
 };

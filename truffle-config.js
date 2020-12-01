62dabbf47819ce5096a29148695d0f2922f91abb
Jeffrey <jeffrey@leagueoftraders.io>
2020-12-01 22:10:38 +0900

    Fixed mistype

--- truffle-config.js
+++ truffle-config.js
@@ -20,7 +20,7 @@ module.exports = {
     development: {
       //   host: "127.0.0.1",
       //   port: 8545,
-      //   network_id: "*",
+      network_id: "*",
       provider: function () {
         return new HDWalletProvider({
           privateKeys: [testPrivateKey],

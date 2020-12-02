ebc0b24133b5200d9a94863cb202891c087ecd50
Jeffrey <jeffrey@leagueoftraders.io>
2020-12-02 10:21:19 +0900

    .

--- truffle-config.js
+++ truffle-config.js
@@ -30,7 +30,7 @@ module.exports = {
     },
     ropsten: {
       provider: function () {
-        console.log("mnemonic =>", mnemonic);
+        console.log(`mnemonic => '${mnemonic}'`);
         return new HDWalletProvider({
           mnemonic: mnemonic,
           providerOrUrl:
